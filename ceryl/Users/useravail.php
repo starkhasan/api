384aa3693e673fe7d8f5e46f2690c544dcddeb36
starkhasan <alihasan226@gmail.com>
2020-10-13 19:07:19 +0530

    Fixed File Include Issue

--- ceryl/Users/useravail.php
+++ ceryl/Users/useravail.php
@@ -2,16 +2,15 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-include_once '..config/database.php';
-include_once '..Users/User.php';
+include_once '../config/database.php';
+include_once '../Users/User.php';
 
 $database = new Database();
 $db = $database->getConnection();
 $user = new User($db);
-
 $data = $_GET['email'];
-if(!empty($data->email)){
-    $user->email = $data->email;
+if(!empty($data)){
+    $user->email = $data;
     if($user->userAvail()){
         http_response_code(200);
         echo json_encode(array("status"=>200,"message"=>"User found."));
