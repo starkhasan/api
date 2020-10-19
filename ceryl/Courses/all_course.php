7c768b76df181e423627695c06f872be26b7ea74
starkhasan <alihasan226@gmail.com>
2020-10-19 21:30:57 +0530

    Add Profile Response  in Course Response

--- ceryl/Courses/all_course.php
+++ ceryl/Courses/all_course.php
@@ -5,23 +5,37 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
-
 include_once '../config/database.php';
 include_once '../Courses/Course.php';
+include_once '../Profiles/Profile.php';
 
 $database = new Database();
 $db = $database->getConnection();
 $course = new Course($db);
-$num = $course->getCourse();
-if(sizeof($num) > 0){
-    $userCourse = array();
-    $userCourse["status"] = 200;
-    $userCourse["message"] = "All User Courses";
-    $userCourse["courses"] = $num;
-    http_response_code(200);
-    echo json_encode($userCourse);
+$profile = new Profile($db);
+$data = $_GET['email'];
+if(!empty($data)){
+    $profile->email = $data;
+    $row = $profile->readProfile();
+    $userProfile = array(
+        "name"=>$row['name'],
+        "email"=>$row['email']
+    );
+    $num = $course->getCourse();
+    if(sizeof($num) > 0){
+        $userCourse = array();
+        $userCourse["status"] = 200;
+        $userCourse["message"] = "All User Courses";
+        $userCourse["profile"] = $userProfile;
+        $userCourse["courses"] = $num;
+        http_response_code(200);
+        echo json_encode($userCourse);
+    }else{
+        http_response_code(200);
+        echo json_encode(array("status"=>400,"message"=>"No Course Available"));
+    }
 }else{
     http_response_code(200);
-    echo json_encode(array("status"=>400,"message"=>"No Course Available"));
+    echo json_encode(array("status"=>400,"message"=>"User not found"));
 }
 ?>
